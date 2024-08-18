e74f0e31e59769f3a79195c7714561c42d703ba1
Randal Dong <ryan.dr@qq.com>
2024-08-18 18:09:35 +0800

    in game menu

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -6,16 +6,23 @@ using UnityEngine.UI;
 
 public class UIManager: MonoBehaviour
 {
+	[Header("For Main Menu Scene")]
+	[SerializeField] private bool isMainMenu;
     [SerializeField] private GameObject cam;
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject settingsPanel;
 	[SerializeField] private GameObject themesPanel;
-
 	[SerializeField] private Slider bgmSlider;
     [SerializeField] private TextMeshProUGUI bgmValueText;
     [SerializeField] private Slider sfxSlider;
 	[SerializeField] private TextMeshProUGUI sfxValueText;
 
+	[Header("For Game Scenes")]
+	[SerializeField] private bool isHud;
+	[SerializeField] private int maxTime = 15;
+	[SerializeField] private TextMeshProUGUI elapsedTimeText;
+	private float elapsedTime;
+	[SerializeField] private GameObject inGameMenu;
 
 	public static UIManager Instance { get; private set; }
 
@@ -34,8 +41,25 @@ public class UIManager: MonoBehaviour
 
 	private void Start()
 	{
-        ChangeBGMValueText();
-        ChangeSFXValueText();
+		if (isMainMenu)
+		{
+			ChangeBGMValueText();
+			ChangeSFXValueText();
+		}
+        
+	}
+
+	private void Update()
+	{
+		if (isHud)
+		{
+			elapsedTime += Time.deltaTime;
+			int minutes = Mathf.FloorToInt(elapsedTime / 60);
+			int seconds = Mathf.FloorToInt(elapsedTime % 60);
+			if (seconds <= maxTime)
+				elapsedTimeText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+		}
+		
 	}
 
 	public void ToggleSettingsPanel()
@@ -78,6 +102,25 @@ public class UIManager: MonoBehaviour
 	public void Theme3Selected()
 	{
 		LevelManager.Instance.LoadCurrentLevel(2, 0);
+	}
+
+	public void ToggleInGameMenu()
+	{
+		if (!inGameMenu.activeSelf)
+		{
+			// pause timer
+			isHud = false;
+			// open menu
+			inGameMenu.SetActive(true);
+		}
+		else
+		{
+			// resume timer
+			isHud=true;
+			// close menu
+			inGameMenu.SetActive(false);
+		}
+		
 	}
 
 	public void ZoomInCam()
