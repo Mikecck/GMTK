7ae6a5cd1d2a9166fa0aac85ffc86f0d7509373f
Mikecck <43435460+Mikecck@users.noreply.github.com>
2024-08-20 23:59:43 +0800

    Level 2.1 Completed

--- Assets/Script/2-1/CheckWalk.cs
+++ Assets/Script/2-1/CheckWalk.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheckWalk : MonoBehaviour
 {
 	public GameObject flamingo1;
 	public GameObject flamingo2;
 	public bool canWalk;
+    [SerializeField]
+    private Button shootButton;
 
-	private void Update()
+    private void Update()
 	{
-		if (flamingo1.transform.position.x > -13)
-			canWalk = flamingo1.activeSelf && flamingo2.activeSelf;
-		else
-			canWalk = false;
-	}
+        if (flamingo1.transform.position.x > -13)
+        {
+            canWalk = flamingo1.activeSelf && flamingo2.activeSelf;
+            shootButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            canWalk = false;
+            shootButton.gameObject.SetActive(false);
+        }
+    }
 	
 }
